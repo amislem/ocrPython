48f8bc5bfb03b3688d8eaf9237e9248d815738a7
tony <tony@localhost.localdomain>
2017-09-26 14:16:54 +0100

    add getter and setter to personne object

--- chap3/personne.py
+++ chap3/personne.py
@@ -1,6 +1,5 @@
 class Personne(object):
-
-    """"Classe définissant une personne caractérisée par :
+    """" Classe définissant une personne caractérisée par :
     - son nom
     - son prénom
     - son âge
@@ -11,15 +10,27 @@ class Personne(object):
         self.nom = "Tony"
         self.prenom = "Spart"
         self.age = 32
-        self.address = "any ware"
+        self._address = "any ware"
 
-        
-class Compteur:
+    def _get_address(self):
+        """Return the address of the person"""
+        print("hello from getter")
+        return self._address
+
+    def _set_address(self, address):
+        """setter for address"""
+        print("hello from setter")
+        self._address = address
+
+    address = property(_get_address, _set_address)
+
+
+class Counter:
     """Cette classe possède un attribut de classe qui s'incrémente à chaque
     fois que l'on crée un objet de ce type"""
 
+    objets_crees = 0  # Le compteur vaut 0 au départ
 
-    objets_crees = 0 # Le compteur vaut 0 au départ
     def __init__(self):
         """À chaque fois qu'on crée un objet, on incrémente le compteur"""
-        Compteur.objets_crees += 1
+        Counter.objets_crees += 1
